3998d92445e2b8370be1a5e5af18afe3848b4ca2
Fadi <fadi.rata@live.fr>
2020-11-17 21:38:18 +0100

    add skeleton loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,49 @@
 <template>
   <v-container>
-    <h1 style="color: #61DAFB">Population Mondiale</h1>
-    <v-row>
+    <cardDetail />
+    <h1 style="color: #61DAFB" class="text-center">Population Mondiale</h1>
+
+    <v-row v-if="loading">
+      <v-col v-for="p in 4" :key="p">
+        <v-skeleton-loader type="card"></v-skeleton-loader>
+      </v-col>
+    </v-row>
+
+    <v-row v-else>
       <v-col v-for="(country, index) in countries" :key="index" class="col-3" max-height="340px">
-        <card :card="country">></card>
+        <card :card="country"></card>
       </v-col>
     </v-row>
 
   </v-container>
 </template>
+
 <script>
+
 import card from '@/components/card'
+import cardDetail from '@/components/cardDetail'
+
 export default {
-  components: { card },
+  components: { card, cardDetail },
   data() {
     return {
-      countries: {}
+      countries: {},
+      loading: false,
     }
   },
   async fetch() {
     try {
+      this.loading = true
       this.countries = await fetch('https://restcountries.eu/rest/v2/all').then(res => res.json())
       console.log(this.countries)
     } catch (error) {
       console.log(error)
     }
+    this.loading = false
   }
 }
 </script>
+
 <style scoped>
 h1 {
   text-transform: uppercase;
